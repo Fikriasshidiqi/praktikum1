83f3418e2f1bac6f4b9c156fa027ca481cdcde93
Fikriasshidiqi <92006081+Fikriasshidiqi@users.noreply.github.com>
2021-10-06 07:20:18 -0800

    Add files via upload
    praktikum 1 
    Nama

---
+++ pwd/Upload/upload1.php
@@ -0,0 +1,6 @@
+
+<form enctype="multipart/form-data" method="post" action="latian_upload1.php">
+File yang diupload: <input type="file"name="fupload"><br>
+Deskripsi File: <br><textarea name="deskripsi" rows="6" cols="20"></textarea><br>
+<input type=submit value=Upload>
+</form>
